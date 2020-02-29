64adfb22300d93f4f958ba2120755f64c6b6880b
Zeeshan Khan <zkhan1093@gmail.com>
2020-02-29 22:09:49 +0530

    adding visit counter

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Counter(models.Model):
+    counter = models.CharField(max_length=100, default='2347234')
--- frontend/views.py
+++ frontend/views.py
@@ -1,28 +1,45 @@
-
 from django.http import HttpResponse
 from django.conf import settings
 import os
 from django.template.loader import get_template
-# Create your views here.
+from frontend.models import Counter
+import random
 
-def index(request, name='index'):
-    template = get_template('index.html')
-    return HttpResponse(template.render())
+random.seed(0)
 
-def about(request, name='about'):
-    template = get_template('about.html')
-    return HttpResponse(template.render())
 
-def categories(request, name='categories'):
-    template = get_template('categories.html')
-    return HttpResponse(template.render())
+def get_count():
+    counter = Counter.objects.first()
+    if counter is None:
+        counter = Counter.objects.create()
+    new_count = int(counter.counter) + random.randint(1, 10)
+    counter.counter = str(new_count)
+    counter.save()
+    return counter.counter
+
+
+def index(request, name="index"):
+    template = get_template("index.html")
+    return HttpResponse(template.render(dict(counter=get_count())))
+
+
+def about(request, name="about"):
+    template = get_template("about.html")
+    return HttpResponse(template.render(dict(counter=get_count())))
+
+
+def categories(request, name="categories"):
+    template = get_template("categories.html")
+    return HttpResponse(template.render(dict(counter=get_count())))
+
 
 def category(request, name=None):
-    template = get_template('category.html')
-    data = dict(name=name)
+    template = get_template("category.html")
+    data = dict(name=name, counter=get_count())
     return HttpResponse(template.render(data))
 
+
 def post(request, postid=None):
-    template = get_template('post.html')
-    data = dict(postid=int(postid))
+    template = get_template("post.html")
+    data = dict(postid=int(postid), counter=get_count())
     return HttpResponse(template.render(data))
