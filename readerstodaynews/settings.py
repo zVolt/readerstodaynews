76a533d430a23fd39c5ab9a8a0b3e64b8c623f4e
Zeeshan Khan <zkhan1093@gmail.com>
2020-03-01 20:40:51 +0530

    fix

--- readerstodaynews/settings.py
+++ readerstodaynews/settings.py
@@ -161,15 +161,8 @@ CLOUDINARY = {
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
-<<<<<<< HEAD
     "http://localhost:8080",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:8080",
-=======
-    "http://127.0.0.1:8000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-
->>>>>>> https -> http
     "https://readertodaynewsfrontend.herokuapp.com",
 ]
