cd984eea314c5ab4f9386448abcc64aaa96c9594
Zeeshan Khan <zkhan1093@gmail.com>
2020-03-12 17:33:17 +0530

    adding new origin for CORS

--- readerstodaynews/settings.py
+++ readerstodaynews/settings.py
@@ -149,9 +149,12 @@ CLOUDINARY = {
 }
 
 CORS_ORIGIN_WHITELIST = [
+    "http://localhost",
     "http://localhost:8000",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:8080",
+    "https://localhost",
     "https://readertodaynewsfrontend.herokuapp.com",
+    "https://readersapi.herokuapp.com",
 ]
