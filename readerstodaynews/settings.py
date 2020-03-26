d80b01d1bc33da3440bf8e72449c1e7f7afd6879
Zeeshan Khan <zkhan1093@gmail.com>
2020-03-26 16:06:38 +0530

    adding middle for counter increase

--- readerstodaynews/settings.py
+++ readerstodaynews/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    "middleware.counter.IncreaseCounterMiddleware",
 ]
 
 ROOT_URLCONF = "readerstodaynews.urls"
